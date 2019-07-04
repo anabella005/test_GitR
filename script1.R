1cb84e5b05d10704200db32edae67d51d343b685
juanchiem <edwardsmolina@gmail.com>
2019-07-04 19:24:05 -0300

    diagnosticos para glm's

--- script1.R
+++ script1.R
@@ -1,3 +1,10 @@
 mod <- lm(len~ dose*supp, ToothGrowth)
 summary(mod)
+par(mfrow=c(2,2))
 plot(mod)
+
+# Glm diagnosticos
+m <- glm(Murder ~ Assault + UrbanPop + Rape,
+         family = gaussian, data = USArrests)
+library(ggfortify)
+autoplot(m, which = 1:6, label.size = 3)
